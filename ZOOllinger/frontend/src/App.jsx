4beda683c9ba749542718783d23d9405ace1b461
Kiara <kiara.baldo@lernende.bbw.ch>
2026-05-05 14:15:40 +0200

    fixed route problem

--- ZOOllinger/frontend/src/App.jsx
+++ ZOOllinger/frontend/src/App.jsx
@@ -1,8 +1,15 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import GetTicketRoute from "./routes/getTicketRoute";
 
 function App() {
-  
-  return <GetTicketRoute />;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<h1>Startseite</h1>} />
+        <Route path="/tickets" element={<GetTicketRoute />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
