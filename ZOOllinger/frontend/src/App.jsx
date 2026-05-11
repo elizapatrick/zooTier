fd5662e3c6efd0ebe22c01a41892675a57ddd244
Eliza Patrick <eliza.pattrick@lernende.bbw.ch>
2026-05-11 20:58:26 +0200

    implemented home-page & added all pictures for all animals and homepage

--- ZOOllinger/frontend/src/App.jsx
+++ ZOOllinger/frontend/src/App.jsx
@@ -1,11 +1,12 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import GetTicketRoute from "./routes/getTicketRoute";
+import { Home } from "./routes/home";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<h1>Startseite</h1>} />
+        <Route path="/" element={<Home />} />
         <Route path="/tickets" element={<GetTicketRoute />} />
       </Routes>
     </BrowserRouter>
