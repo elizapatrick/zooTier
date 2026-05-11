f701ff52b9ae83abb6014e61e88c976dae139f9f
mikolaj.spychala <spychalamikolaj2@gmail.com>
2026-05-12 00:25:56 +0200

    implemented visitor experience site with corresponding components

--- ZOOllinger/frontend/src/App.jsx
+++ ZOOllinger/frontend/src/App.jsx
@@ -1,5 +1,6 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import GetTicketRoute from "./routes/getTicketRoute";
+import ExperienceRoute from "./routes/ExperienceRoute";
 
 function App() {
   return (
@@ -7,6 +8,7 @@ function App() {
       <Routes>
         <Route path="/" element={<h1>Startseite</h1>} />
         <Route path="/tickets" element={<GetTicketRoute />} />
+        <Route path="/experience" element={<ExperienceRoute />} />
       </Routes>
     </BrowserRouter>
   );
