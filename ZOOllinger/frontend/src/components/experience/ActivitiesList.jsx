34ba971580dff4d9cc77995ae6d6e162ca0e2237
mikolaj.spychala <spychalamikolaj2@gmail.com>
2026-05-12 08:47:11 +0200

    added activities for visitor experience

--- ZOOllinger/frontend/src/components/experience/ActivitiesList.jsx
+++ ZOOllinger/frontend/src/components/experience/ActivitiesList.jsx
@@ -1,5 +1,6 @@
 import ActivityCard from "./ActivityCard"
-import zooLogo from "../../assets/ZooLogo.png"
+import elephantImg from "../../assets/elephant.png"
+import zebraImg from "../../assets/zebra.png"
 
 export default function ActivitiesList() {
   return (
@@ -9,7 +10,7 @@ export default function ActivitiesList() {
         <p>All activities are included with your ticket</p>
 
         <ActivityCard
-          image={zooLogo}
+          image={elephantImg}
           title="Elephant Feeding Observation"
           location="Savanna Zone, Area B"
           description="Watch our majestic elephants enjoy their meal and learn about their diet."
@@ -18,6 +19,18 @@ export default function ActivitiesList() {
             { name: "Dumbo", age: "25 years old" },
             { name: "Ella", age: "32 years old" },
             { name: "Ellie", age: "8 years old" },
+          ]}
+        />
+        <ActivityCard
+          image={zebraImg}
+          title="Zebra Feeding Observation"
+          location="Savanna Zone, Area B"
+          description="Watch our majestic Zebras enjoy their meal and learn about their diet."
+          times={["10:30", "14:00", "16:30"]}
+          animals={[
+            { name: "Bambo", age: "25 years old" },
+            { name: "Anna", age: "32 years old" },
+            { name: "Hanna", age: "8 years old" },
           ]}
         />
       </div>
